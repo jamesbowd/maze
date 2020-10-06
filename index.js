7b363854de349e28b7a3671f690bbaa52c905534
jamesbowd <jamesbowd@gmail.com>
2020-10-06 19:18:12 +0100

    create walls to contain objects

---
+++ index.js
@@ -0,0 +1,30 @@
+//Setup for matter.js
+
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: 800,
+    height: 600,
+  },
+});
+
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+// Walls
+
+const walls = [
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+];
+
+// Add to world
+
+World.add(world, walls);
