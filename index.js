7e8a7b53b50f4511141e286327071e44ac998357
jamesbowd <jamesbowd@gmail.com>
2020-10-08 19:36:52 +0100

    create grid. Started logic to create maze

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 //Setup for matter.js
 
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
+
+const cells = 3;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -19,30 +21,79 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-  world,
-  MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas),
-  })
-);
-
 // Walls
 
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
 ];
 
 World.add(world, walls);
 
-// Random shapes
+// Maze generation
 
-for (let i = 0; i < 20; i++) {
-  if (Math.random() > 0.5) {
-    World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
-  } else {
-    World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35));
+const shuffle = (arr) => {
+  let counter = arr.length;
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+
+    counter--;
+
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
   }
-}
+  return arr;
+};
+
+// create an array with 3 arrays with 3 false values (not been visited)
+const grid = Array(cells)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
+
+const horizantals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+  // If I have visited the cell at [row, column], then return
+  if (grid[row][column]) {
+    return;
+  }
+
+  // Mark this cell as being visited
+  grid[row][column] = true;
+
+  // Assemble randomly ordered list of neighbours
+
+  const neighbours = shuffle([
+    [row - 1, column],
+    [row, column - 1],
+    [row + 1, column],
+    [row, column + 1],
+  ]);
+
+  console.log(neighbours);
+
+  // For each neighbour ...
+
+  // See if that neighbour is out of bounds
+
+  // If we have visited that neighbour, continue to next neighbour
+
+  // Remove wall from horizontal or vertical array
+
+  // Visit that next cell
+};
+
+stepThroughCell(1, 1);
